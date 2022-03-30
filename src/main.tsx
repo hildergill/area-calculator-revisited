37930d18621337c8402c8fdd098d08bea45f7169
hildergill <hildergill@gmail.com>
2022-03-30 14:01:53 +0800

    Some corrections

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
+import App from "./styles/components/App";
 
 import "./styles/common.scss";
 
-ReactDOM.render(<></>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
