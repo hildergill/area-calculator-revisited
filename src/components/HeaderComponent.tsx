6f70cd61a73507b6e5fbcf7e1297ed2ebe721db3
hildergill <hildergill@gmail.com>
2022-03-31 09:03:04 +0800

    Some backup commit

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,10 +1,14 @@
 import { useTranslation } from "react-i18next";
 
+import HeaderComponentStyles from "../styles/HeaderComponent.module.scss";
+
 const HeaderComponent = () => {
 	const { t } = useTranslation();
 
+	const { headerComponent } = HeaderComponentStyles;
+
 	return (
-		<header>
+		<header className={headerComponent}>
 			<h1>{t("common:appName")}</h1>
 		</header>
 	);
