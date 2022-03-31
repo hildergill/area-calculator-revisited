89ff83a017fd3649d783aeb88e8071ec03d909f6
hildergill <hildergill@gmail.com>
2022-03-31 18:27:59 +0800

    Added the shape switching feature

--- src/components/ShapeSelector.tsx
+++ src/components/ShapeSelector.tsx
@@ -3,14 +3,30 @@ import ShapeCollection from "../shapecollection";
 
 import ShapeSelectorStyles from "../styles/ShapeSelector.module.scss";
 
-const ShapeSelector = () => {
+type OnSelectedShapeSelector = {
+	(index: number): void;
+};
+
+type ShapeSelectorProps = {
+	activeIndex: number;
+	onSelected?: OnSelectedShapeSelector;
+};
+
+const ShapeSelector = (props: ShapeSelectorProps) => {
 	const { t } = useTranslation();
 
-	const { shapeSelector } = ShapeSelectorStyles;
+	const { shapeSelector, shapeSelectorButtonBase, shapeSelectorButtonActive } = ShapeSelectorStyles;
 
-	const shapeSelectorButtons: JSX.Element[] = Object.values(ShapeCollection).map((shape) => {
+	const shapeSelectorButtons: JSX.Element[] = Object.values(ShapeCollection).map((shape, key) => {
+		const { activeIndex, onSelected }: ShapeSelectorProps = props,
+			styleString: string = [shapeSelectorButtonBase, activeIndex === key ? shapeSelectorButtonActive : ""].join(" ");
+
+		const onSelectedHandler = () => {
+			if (onSelected) onSelected(key);
+		};
+
 		return (
-			<button>
+			<button className={styleString} key={key} onClick={onSelectedHandler}>
 				<p>{shape.getShapeName(t)}</p>
 			</button>
 		);
