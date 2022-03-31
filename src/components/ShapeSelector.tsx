33c1b53fb49ad04dde9697ef3c87b4a2b9e8db14
hildergill <hildergill@gmail.com>
2022-03-31 17:59:24 +0800

    Added the shape selector stuff

--- src/components/ShapeSelector.tsx
+++ src/components/ShapeSelector.tsx
@@ -1,8 +1,12 @@
 import { useTranslation } from "react-i18next";
 import ShapeCollection from "../shapecollection";
 
+import ShapeSelectorStyles from "../styles/ShapeSelector.module.scss";
+
 const ShapeSelector = () => {
 	const { t } = useTranslation();
+
+	const { shapeSelector } = ShapeSelectorStyles;
 
 	const shapeSelectorButtons: JSX.Element[] = Object.values(ShapeCollection).map((shape) => {
 		return (
@@ -12,7 +16,7 @@ const ShapeSelector = () => {
 		);
 	});
 
-	return <nav>{shapeSelectorButtons}</nav>;
+	return <nav className={shapeSelector}>{shapeSelectorButtons}</nav>;
 };
 
 export default ShapeSelector;
--- src/shapecollection.tsx
+++ src/shapecollection.tsx
@@ -7,12 +7,17 @@ type ShapeType<Params> = {
 
 type ShapeCollectionType = {
 	circleRadius: ShapeType<{ radius: number }>;
+	circleDiameter: ShapeType<{ diameter: number }>;
 };
 
 const ShapeCollection: ShapeCollectionType = {
 	circleRadius: {
 		getShapeName: (t: TFunction) => t("shapes:circleRadius:name"),
 		calculateArea: ({ radius }) => 2 * Math.PI * radius
+	},
+	circleDiameter: {
+		getShapeName: (t: TFunction) => t("shapes:circleDiameter:name"),
+		calculateArea: ({ diameter }) => Math.PI * diameter
 	}
 };
 
