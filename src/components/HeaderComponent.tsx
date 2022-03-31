ef1c4e5ab8c4557be045be80f4e5e4eabd971b80
hildergill <hildergill@gmail.com>
2022-03-31 11:19:30 +0800

    This is good

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -7,13 +7,13 @@ import HeaderComponentStyles from "../styles/HeaderComponent.module.scss";
 const HeaderComponent = () => {
 	const { t } = useTranslation();
 
-	const { headerComponent } = HeaderComponentStyles;
+	const { headerComponent, repoLink } = HeaderComponentStyles;
 
 	return (
 		<header className={headerComponent}>
 			<h1>{t("common:appName")}</h1>
 
-			<a href={repository} target="_blank" title={t("headerComponent:visitGithubRepository")}>
+			<a className={repoLink} href={repository} target="_blank" title={t("headerComponent:visitGithubRepository")}>
 				<IconBrandGithub />
 				<p>{t("headerComponent:visitGithubRepository")}</p>
 			</a>
