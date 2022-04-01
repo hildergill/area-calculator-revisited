8a0c9b568678ddc9906c4eecd2caf49eb7552cf9
hildergill <hildergill@gmail.com>
2022-04-01 19:43:52 +0800

    Backup commit

--- src/components/CalculatorForm.tsx
+++ src/components/CalculatorForm.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import ShapeCollection from "../shapecollection";
 
 import CalculatorFormStyle from "../styles/CalculatorForm.module.scss";
+import GenericButtonStyles from "../styles/GenericButton.module.scss";
 
 type CalculatorFormProps = {
 	activeIndex: number;
@@ -14,12 +15,13 @@ const CalculatorForm = (props: CalculatorFormProps) => {
 
 	const { getCalculatorForm } = Object.values(ShapeCollection)[activeIndex];
 
-	const { calculatorForm } = CalculatorFormStyle;
+	const { calculatorForm } = CalculatorFormStyle,
+		{ genericButton } = GenericButtonStyles;
 
 	return (
 		<form className={calculatorForm}>
 			{getCalculatorForm(t)}
-			<input type="submit" value={String(t("calculatorForm:calculateButton"))} />
+			<input type="submit" className={genericButton} value={String(t("calculatorForm:calculateButton"))} />
 		</form>
 	);
 };
