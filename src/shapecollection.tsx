11485aa25cfb8d3c296e3e68474c30d3a68021e6
hildergill <hildergill@gmail.com>
2022-04-02 11:37:24 +0800

    Added new shapes

--- src/shapecollection.tsx
+++ src/shapecollection.tsx
@@ -11,6 +11,9 @@ type ShapeType<Params> = {
 type ShapeCollectionType = {
 	circleRadius: ShapeType<{ radius: number }>;
 	circleDiameter: ShapeType<{ diameter: number }>;
+	triangle: ShapeType<{ width: number; height: number }>;
+	square: ShapeType<{ width: number }>;
+	rectangle: ShapeType<{ width: number; height: number }>;
 };
 
 const ShapeCollection: ShapeCollectionType = {
@@ -33,6 +36,47 @@ const ShapeCollection: ShapeCollectionType = {
 			<>
 				<label htmlFor="diameter">{t("shapes:circleDiameter.inputs.diameter")}</label>
 				<NumberInput name="diameter" />
+			</>
+		)
+	},
+	triangle: {
+		getShapeName: (t: TFunction) => t("shapes:triangle:name"),
+		calculateArea: ({ width, height }) => 0.5 * width * height,
+		getFormula: () => <>A = &frac12; &middot; w &middot; h</>,
+		getCalculatorForm: (t: TFunction) => (
+			<>
+				<label htmlFor="width">{t("shapes:triangle.inputs.width")}</label>
+				<NumberInput name="width" />
+				<label htmlFor="height">{t("shapes:triangle.inputs.height")}</label>
+				<NumberInput name="height" />
+			</>
+		)
+	},
+	square: {
+		getShapeName: (t: TFunction) => t("shapes:square:name"),
+		calculateArea: ({ width }) => Math.pow(width, 2),
+		getFormula: () => (
+			<>
+				A = w<sup>2</sup>
+			</>
+		),
+		getCalculatorForm: (t: TFunction) => (
+			<>
+				<label htmlFor="width">{t("shapes:square.inputs.width")}</label>
+				<NumberInput name="width" />
+			</>
+		)
+	},
+	rectangle: {
+		getShapeName: (t: TFunction) => t("shapes:rectangle:name"),
+		calculateArea: ({ width, height }) => width * height,
+		getFormula: () => <>A = w &middot; h</>,
+		getCalculatorForm: (t: TFunction) => (
+			<>
+				<label htmlFor="width">{t("shapes:rectangle.inputs.width")}</label>
+				<NumberInput name="width" />
+				<label htmlFor="height">{t("shapes:rectangle.inputs.height")}</label>
+				<NumberInput name="height" />
 			</>
 		)
 	}
