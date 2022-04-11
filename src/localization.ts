ed26fb2d0135ed1bd244a68f52cbc21cc3942d5e
hildergill <hildergill@gmail.com>
2022-04-11 22:23:35 +0800

    Added the Simplified Chinese localization

--- src/localization.ts
+++ src/localization.ts
@@ -12,12 +12,23 @@ import HeaderComponentZh from "../locales/zh/headercomponent.json";
 import ShapesZh from "../locales/zh/shapes.json";
 import CalculatorFormZh from "../locales/zh/calculatorform.json";
 
+import CommonZhCN from "../locales/zh-CN/common.json";
+import HeaderComponentZhCN from "../locales/zh-CN/headercomponent.json";
+import ShapesZhCN from "../locales/zh-CN/shapes.json";
+import CalculatorFormZhCN from "../locales/zh-CN/calculatorform.json";
+
 export const resources: Resource = {
 	zh: {
 		common: CommonZh,
 		headerComponent: HeaderComponentZh,
 		shapes: ShapesZh,
 		calculatorForm: CalculatorFormZh
+	},
+	"zh-CN": {
+		common: CommonZhCN,
+		headerComponent: HeaderComponentZhCN,
+		shapes: ShapesZhCN,
+		calculatorForm: CalculatorFormZhCN
 	},
 	en: {
 		common: CommonEn,
@@ -31,6 +42,6 @@ i18next
 	.use(initReactI18next)
 	.use(I18nextBrowserLanguageDetector)
 	.init({
-		supportedLngs: ["zh", "en"],
+		supportedLngs: ["zh", "zh-CN", "en"],
 		resources
 	});
