3154e73eff115685a947f2b0cb4a6cd25b8e7471
hildergill <hildergill@gmail.com>
2022-04-20 20:32:23 +0800

    Added more copyright header

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+// This file is a part of area-calculator-revisited
+// https://www.github.com/hildergill/area-calculator-revisited
+// Copyright 2022 Hilder Gill
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
