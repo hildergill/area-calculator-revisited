fe0e7e838ec5e2a90a50aa9b0e8e400ecfc8921f
hildergill <hildergill@gmail.com>
2022-04-23 23:12:58 +0800

    Readded the Chinese locale files

--- src/localization.ts
+++ src/localization.ts
@@ -6,12 +6,16 @@ import i18next, { Resource } from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import en from "../locales/en.json";
+import zh from "../locales/zh.json";
+import zhCn from "../locales/zh-cn.json";
 import es from "../locales/es.json";
+import en from "../locales/en.json";
 
 export const resources: Resource = {
 	en,
-	es
+	es,
+	zh,
+	"zh-CN": zhCn
 } as const;
 
 i18next
@@ -19,6 +23,6 @@ i18next
 	.use(I18nextBrowserLanguageDetector)
 	.init({
 		resources,
-		supportedLngs: ["en", "es"],
+		supportedLngs: ["zh", "zh-CN", "es", "en"],
 		fallbackLng: "en"
 	});
