c35151e3bc43f8d73d447540f2de67385fb127f7
hildergill <hildergill@gmail.com>
2022-05-07 09:32:47 +0800

    Added the base rule to the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	base: "https://hildergill.github.io/area-calculator-revisited/"
 });
