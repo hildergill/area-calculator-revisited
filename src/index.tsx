425e79274b3fd81b048d6f2e33e173ac7590f5ff
hildergill <hildergill@gmail.com>
2022-06-12 09:21:31 +0800

    Added the global stylesheets

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Root, createRoot } from "react-dom/client";
+import "./stylesheets/global.css";
 
 const root: Root = createRoot(document.getElementById("root")!);
 root.render(<h1>🌎 Hello, World!</h1>);
