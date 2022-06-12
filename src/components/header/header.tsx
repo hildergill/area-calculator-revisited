7bcb0f03f44ee938f479b13c2bf62e4e7bf1a59a
hildergill <hildergill@gmail.com>
2022-06-12 17:58:39 +0800

    Added the common stylesheets stuff

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,13 +2,14 @@ import { FC } from "react";
 import { IconBrandGithub } from "@tabler/icons";
 import Package from "../../../package.json";
 import HeaderStyles from "./header.module.css";
+import CommonStyles from "../../common.module.css";
 
 export const Header: FC = () => {
 	return (
 		<header className={HeaderStyles.header}>
 			<h1>{Package.displayName}</h1>
 
-			<a target={"_blank"} href={Package.repository}>
+			<a target={"_blank"} href={Package.repository} className={CommonStyles.commonButton}>
 				<IconBrandGithub />
 				<p>Visit GitHub Repository</p>
 			</a>
