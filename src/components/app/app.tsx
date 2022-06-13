cbb49a52982ce6f578428f8ac710b5f4c2d66522
hildergill <hildergill@gmail.com>
2022-06-13 19:58:59 +0800

    Added the CalculatorForm contents

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,7 @@ import { Header } from "../header/header";
 import { ShapeSelector } from "../shapeselector/shapeselector";
 import AppStyles from "./app.module.css";
 import { OnSelectShapeButton } from "../shapeselector/shapeselector";
+import { CalculatorForm } from "../calculatorform/calculatorform";
 
 export const App: FC = () => {
 	const [activeShape, setActiveShape] = useState<number>(0);
@@ -15,6 +16,7 @@ export const App: FC = () => {
 		<div className={AppStyles.app}>
 			<Header />
 			<ShapeSelector activeShape={activeShape} onSelectShapeButton={onSelectShapeSelector} />
+			<CalculatorForm activeShape={activeShape} />
 		</div>
 	);
 };
--- src/components/calculatorform/calculatorform.tsx
+++ src/components/calculatorform/calculatorform.tsx
@@ -1,11 +1,35 @@
-import { FC } from "react";
+import { FC, FormEvent } from "react";
+import { Shapes } from "../../shapes";
+import { IconCheck, IconX } from "@tabler/icons";
 
-export const CalculatorForm: FC = () => {
+export type CalculatorFormProps = {
+	activeShape: number;
+};
+
+export const CalculatorForm: FC<CalculatorFormProps> = (props: CalculatorFormProps) => {
+	const { activeShape }: CalculatorFormProps = props;
+
+	const { getName } = Object.values(Shapes)[activeShape];
+
 	return (
 		<main>
-			{/* TODO Add something here later */}
-			{/* TODO Add something here later */}
-			{/* TODO Add something here later */}
+			<h2>{getName()}</h2>
+
+			<form onSubmit={(event: FormEvent) => event.preventDefault()}>
+				{/* TODO Add something here later */}
+				{/* TODO Add something here later */}
+				{/* TODO Add something here later */}
+
+				<button type="reset">
+					<p>Reset</p>
+					<IconX />
+				</button>
+
+				<button type="submit">
+					<p>Submit</p>
+					<IconCheck />
+				</button>
+			</form>
 		</main>
 	);
 };
