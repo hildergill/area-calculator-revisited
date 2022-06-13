0765dab1533c6ec6b0a3794e5937ae2fc7111705
hildergill <hildergill@gmail.com>
2022-06-13 19:30:03 +0800

    Added the CalculatorForm component file

---
+++ src/components/calculatorform/calculatorform.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const CalculatorForm: FC = () => {
+	return (
+		<main>
+			{/* TODO Add something here later */}
+			{/* TODO Add something here later */}
+			{/* TODO Add something here later */}
+		</main>
+	);
+};
