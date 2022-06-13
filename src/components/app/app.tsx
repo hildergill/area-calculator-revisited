d840c86d5a31da0b394984430b6ebb074630f19d
hildergill <hildergill@gmail.com>
2022-06-13 08:31:21 +0800

    Added the ShapeSelector stylesheets

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Header } from "../header/header";
+import { ShapeSelector } from "../shapeselector/shapeselector";
 import AppStyles from "./app.module.css";
 
 export const App: FC = () => {
 	return (
 		<div className={AppStyles.app}>
 			<Header />
+			<ShapeSelector activeShape={0} />
 		</div>
 	);
 };
--- src/components/shapeselector/shapeselector.tsx
+++ src/components/shapeselector/shapeselector.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
 import { Shapes, Shape } from "../../shapes";
+import ShapeSelectorStyles from "./shapeselector.module.css";
 
-export const ShapeSelector: FC = () => {
+export type ShapeSelectorProps = {
+	activeShape: number;
+};
+
+export const ShapeSelector: FC<ShapeSelectorProps> = (props: ShapeSelectorProps) => {
+	const { activeShape }: ShapeSelectorProps = props;
+
 	const shapeArray = Object.values(Shapes);
 
 	const shapeButtons: JSX.Element[] = shapeArray.map((shape: Shape<any>, key: number) => {
 		return <button key={key}>{shape.getName()}</button>;
 	});
 
-	return <nav>{shapeButtons}</nav>;
+	return <nav className={ShapeSelectorStyles.shapeSelector}>{shapeButtons}</nav>;
 };
