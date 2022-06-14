bfada32f43a6f0f703a881b70bed52867f72ac06
hildergill <hildergill@gmail.com>
2022-06-14 11:22:26 +0800

    Added the ResultsList stylesheets

--- src/components/resultslist/resultslist.tsx
+++ src/components/resultslist/resultslist.tsx
@@ -1,11 +1,26 @@
 import { FC } from "react";
+import ResultsListStyles from "./resultslist.module.css";
 
-export const ResultsList: FC = () => {
-	return (
-		<output>
-			{/* TODO Add something here later */}
-			{/* TODO Add something here later */}
-			{/* TODO Add something here later */}
-		</output>
-	);
+export type ResultType = {
+	params: any;
+	area: number;
+};
+
+export type ResultsListProps = {
+	results: ResultType[];
+};
+
+export const ResultsList: FC<ResultsListProps> = (props: ResultsListProps) => {
+	const { results }: ResultsListProps = props;
+
+	const resultsBox: JSX.Element[] = results.map((results: ResultType, key: number) => {
+		return (
+			<div key={key}>
+				<p></p>
+				<strong>{results.area}</strong>
+			</div>
+		);
+	});
+
+	return <output className={ResultsListStyles.resultsList}>{resultsBox}</output>;
 };
