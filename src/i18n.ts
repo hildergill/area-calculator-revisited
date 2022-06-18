2c7a54b5a7db6c1314cbada3d431892daa5dc65c
hildergill <hildergill@gmail.com>
2022-06-18 19:00:51 +0800

    Added the copyright header

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,3 +1,7 @@
+// This file is a part of area-calculator-revisited
+// https://www.github.com/hildergill/area-calculator-revisited
+// Copyright 2022 Hilder Gill <hildergill@gmail.com>
+
 import i18next, { InitOptions } from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
