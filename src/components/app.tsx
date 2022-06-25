2d1b759b4e5dc99e0a340da141a3eeacbf6306e8
hildergill <hildergill@gmail.com>
2022-06-25 08:11:22 +0800

    Some code cleanup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,5 @@
 // This file is a part of area-calculator-revisited
-// https://www.github.com/hildergill/area-calculator-revisited
+// https://github.com/hildergill/area-calculator-revisited
 // Copyright 2022 Hilder Gill <hildergill@gmail.com>
 
 import { FC, useState } from "react";
